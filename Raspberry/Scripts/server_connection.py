8140e201b77894b3532e321902578ce9d7b4e637
Rajendra Kadam <rajendrakadam249@gmail.com>
2017-03-08 21:28:28 +0530

    Add gitignore

---
+++ Raspberry/Scripts/server_connection.py
@@ -0,0 +1,6 @@
+# This is the file responsible for making connection request to server
+# This paclage has a Request class which takes (url, data, content_type) as params
+import urllib2
+
+def send_image_data(url):
+	
