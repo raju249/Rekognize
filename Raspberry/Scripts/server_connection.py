95a14a4837ada07ba910f75f557c6ae19ffa8b72
Rajendra Kadam <rajendrakadam249@gmail.com>
2017-03-11 15:40:59 +0530

    Add server connection code

--- Raspberry/Scripts/server_connection.py
+++ Raspberry/Scripts/server_connection.py
@@ -1,6 +1,12 @@
 # This is the file responsible for making connection request to server
 # This paclage has a Request class which takes (url, data, content_type) as params
 import urllib2
+import os
 
-def send_image_data(url):
-	
+def send_image_data_and_play_audio(url, data):
+	req = urllib2.Request(url, data, {'Content-Type': 'application/json'})
+	audio_file = urllib2.urlopen(req)
+	with open('test.mp3','wb') as output:
+ 		output.write(audio_file.read())
+ 	# Yet to figure out to play data.
+
